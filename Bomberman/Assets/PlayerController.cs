11f4453cb5e4c7d950615a0456ff4a494a19793a
Andrew Nattress <CSG17009550@HAL.local>
2019-10-30 20:16:07 +0000

    init

---
+++ Bomberman/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController rb;
+    public float speed = 8f;
+
+    void Start()
+    {
+        rb = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rb.Move(input * speed * Time.deltaTime);
+    }
+}
